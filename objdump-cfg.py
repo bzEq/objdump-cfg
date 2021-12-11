621c2627e43170968451ff738dfe106fcc39ce5c
Kai Luo <gluokai@gmail.com>
2021-12-11 14:35:10 +0800

    Add jmpq to uncondbr

--- objdump-cfg.py
+++ objdump-cfg.py
@@ -16,8 +16,20 @@ FUNCTION_BEGIN_LINE = re.compile(r'^([0-9a-f]+)\s+<(.+)>:$')
 INSTRUCTION_LINE = re.compile(r'^\s*([0-9a-f]+):\s*(.*)$')
 INSTRUCTION = re.compile(r'([^<#]+)(<([^\+]+)(\+([0-9a-fx]+))?>)?')
 
-UNCONDITIONAL_BRANCHES = [re.compile(x) for x in [r'\bb\b', r'\bjmp\b']]
-LONG_BRANCHES = [re.compile(x) for x in [r'\bblr\b', r'\bretq\b', r'\bret\b']]
+UNCONDITIONAL_BRANCHES = [
+    re.compile(x) for x in [
+        r'\bb\b',
+        r'\bjmp\b',
+        r'\bjmpq\b',
+    ]
+]
+LONG_BRANCHES = [
+    re.compile(x) for x in [
+        r'\bblr\b',
+        r'\bretq\b',
+        r'\bret\b',
+    ]
+]
 
 
 def IsUncondBr(s):
